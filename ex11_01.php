18c5a237f5c10bd759fc15624c91e06156a0d8b9
hitourebito <yuto0423777@gmail.com>
2019-10-24 12:23:06 +0900

    add session_start in title

--- ex11_01.php
+++ ex11_01.php
@@ -2,7 +2,16 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title></title>
+  <title>
+<?php
+  session_start();
+  $_SESSION["time"] = time();
+  $_SESSION["session_id"] = session_id();
+
+  header("Location: ex11_02.php");
+  exit;
+?>
+  </title>
 </head>
 <body>
 </body>
